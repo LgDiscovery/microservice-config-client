f6450f6b261b80a6cf6cd89a919de454f04b2b49
lg <1475687119@qq.com>
2019-02-06 10:53:37 +0800

    Initial commit

---
+++ src/main/java/ConfigClientApplication.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.lg"})
+public class ConfigClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class,args);
+    }
+}
